b52df0bb9b4fe4c848694d4ce3181ac3165ce8f1
Michael Lujan <35704428+mal290@users.noreply.github.com>
2018-03-27 12:21:27 -0500

    Added comment with my name in it

--- src/main/java/com/hundredwordsgof/flyweight/Flyweight.java
+++ src/main/java/com/hundredwordsgof/flyweight/Flyweight.java
@@ -1,5 +1,5 @@
 package com.hundredwordsgof.flyweight;
-
+// This is my comment with Michael Lujan's name in it.
 /**
  * 
  * Flyweight, interface through flyweight can receive and act on extrinsic
